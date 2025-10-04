226d1f60b3cd083e8eece25829efb1f59920f9df
damdavidprieto <dam.david.prieto@gmail.com>
2025-10-04 22:28:46 +0200

    Update logout.js

--- private/js/logout.js
+++ private/js/logout.js
@@ -1,11 +1,30 @@
+// logout.js
+
+// Asegúrate de tener Firebase importado y configurado igual que en login.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBh6tdi3NswyHj4RVNfIEGYIP9CoMe-BsQ",
+  authDomain: "sepultururosvercelapp.firebaseapp.com",
+  projectId: "sepultururosvercelapp",
+  storageBucket: "sepultururosvercelapp.appspot.com",
+  messagingSenderId: "4986417399",
+  appId: "1:4986417399:web:537bc902d6037dbc9d23f2",
+  measurementId: "G-BYSTFYZPBJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 function logout() {
-  // Hacer logout Firebase y limpiar cookie backend
+  // Primero limpias la cookie backend
   fetch('/api/clear-token', {
     method: 'POST',
     credentials: 'include'
   }).finally(() => {
+    // Luego haces logout Firebase
     auth.signOut().then(() => {
       window.location.href = "/";
     });
   });
 }
+
+// Exportar o conectar con botón según cómo uses este script
